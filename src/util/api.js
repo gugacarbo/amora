1bc8a374145562814a7032ae3645b58fb2ca202e
gugacarbo <gustavo@digitalscudero.com.br>
2022-11-08 22:40:08 -0300

    responsive start

--- src/util/api.js
+++ src/util/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://www.digitalscudero.com.br/amora/api",
+  baseURL: "https://www.digitalscudero.com.br/amora/api",
   // baseURL: "http://localhost/api",
-  baseURL: "http://192.168.16.100/api",
+//   baseURL: "http://192.168.16.100/api",
 });
 
 export default api;
